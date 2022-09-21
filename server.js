1fd403c872e06b60960291345ecd9fb893d7b054
caowens <caowens555@yahoo.com>
2022-09-20 21:30:06 -0500

    Add students/1 route

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
     console.log('GET /');
+    res.send('Hi');
+});
+
+app.get('/students', (req, res) => {
+    console.log('GET /');
     res.render('students');
+});
+
+app.get('/students/1', (req, res) => {
+    console.log('GET /');
+    res.render('student1');
 });
 
 app.listen(3001);
